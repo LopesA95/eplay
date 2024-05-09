a3c682c8ec01902774ed78c9715b8910f20286bd
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-05-09 12:55:51 -0400

    ✨ Refactor styles and components for improved responsiveness and Checkout page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import { Product } from './pages/Product'
 import { Categories } from './pages/Categories'
+import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
+import { Product } from './pages/Product'
 
 export const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
     <Route path="/product/:id" element={<Product />} />
+    <Route path="/checkout" element={<Checkout />} />
   </Routes>
 )
